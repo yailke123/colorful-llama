9569ecb57c5e1469d4c3fc4dbea17eb3edfac432
ege <e.ge.ozcan1907@hotmail.com>
2021-01-30 16:09:46 +0300

    Move playermovement to correct location and add jumping logic

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,7 +4,9 @@ public class PlayerMovement : MonoBehaviour
 {
     public CharacterController2D controller2D;
     public float runSpeed = 40f;
+    public float jumpMultiplier = 10f; //TODO tweak
     private float horizontalMove = 0f;
+    private float verticalMove = 0f;
     
     // Start is called before the first frame update
     void Start()
@@ -16,10 +18,12 @@ public class PlayerMovement : MonoBehaviour
     void Update()
     {
         horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
+        verticalMove = Input.GetAxisRaw("Vertical") * jumpMultiplier;
     }
 
     private void FixedUpdate()
     {
-        controller2D.Move(horizontalMove * Time.fixedDeltaTime,false,false);
+        controller2D.Move(horizontalMove * Time.fixedDeltaTime, false, false);
+        controller2D.Move(verticalMove * Time.fixedDeltaTime, false, true);
     }
 }

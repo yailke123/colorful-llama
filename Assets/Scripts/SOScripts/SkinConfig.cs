92a99ad87b504ffe6b359ae5fae2a16152469fdc
murattuver <murattuver@gmail.com>
2021-01-30 16:04:28 +0300

    Add: SkinManager for future skin support Add: Initial spit script

---
+++ Assets/Scripts/SOScripts/SkinConfig.cs
@@ -0,0 +1,9 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "SkinConfig", menuName = "Config/SkinConfig", order = 0)]
+public class SkinConfig : ScriptableObject {
+    [SerializeField] private Sprite spitSprite;
+    
+        
+        
+}

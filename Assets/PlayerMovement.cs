7f6b5ec4f4823a2baacd4f4ecdccc546985e1217
Yagiz Efe Mertol <yailke123@gmail.com>
2021-01-30 15:38:50 +0300

    tile setup

---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public CharacterController2D controller2D;
+    public float runSpeed = 40f;
+    private float horizontalMove = 0f;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
+    }
+
+    private void FixedUpdate()
+    {
+        controller2D.Move(horizontalMove * Time.fixedDeltaTime,false,false);
+    }
+}

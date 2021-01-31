c7c2dd8378f6938bf19a33ac8e8f99276d441b1e
Yagiz Efe Mertol <yailke123@gmail.com>
2021-01-31 14:34:44 +0300

    rainnn

--- Assets/Scripts/Managers/SessionManager.cs
+++ Assets/Scripts/Managers/SessionManager.cs
@@ -1,6 +1,23 @@
+using System;
+using System.Collections;
+using DigitalRuby.RainMaker;
 using UnityEngine;
 
 public class SessionManager : MonoBehaviour {
     [SerializeField] private SkinManager skinManager;
+    [SerializeField] private PostProcessingManager postProcessingManager;
+    [SerializeField] private GameObject rainObj;
 
+    private void Start()
+    {
+        StartCoroutine("RainRoutine");
+    }
+
+    private IEnumerator RainRoutine()
+    {
+        yield return new WaitForSeconds(1);
+        postProcessingManager.DesaturateEverything();
+        yield return new WaitForSeconds(1);
+        rainObj.SetActive(false);
+    }
 }

2ac4b3b10d4ae5194a24ed5cfc9195a8b81dc6e4
Yagiz Efe Mertol <yailke123@gmail.com>
2021-01-31 12:39:56 +0300

    color addition method

--- Assets/Scripts/Managers/PostProcessingManager.cs
+++ Assets/Scripts/Managers/PostProcessingManager.cs
@@ -4,29 +4,60 @@ using UnityEngine.Rendering.PostProcessing;
 
 public class PostProcessingManager : MonoBehaviour {
     [SerializeField] private PostProcessVolume globalPP;
-    private PostProcessProfile _postProcessProfile;
+    [SerializeField] private PostProcessProfile[] profiles;
+    
+    private PostProcessProfile _globalPostProcessProfile;
+    private ColorGrading _globalColorGrading;
+
+    private bool _colorVirginity = true;
+
 
     private void Awake() {
-        _postProcessProfile = globalPP.profile;
+        _globalPostProcessProfile = globalPP.profile;
     }
 
-    public void DeSaturateGreen() {
-        ColorGrading colorGrading;
-        _postProcessProfile.TryGetSettings(out colorGrading);
+    private void Start()
+    {
+        _globalPostProcessProfile.TryGetSettings(out _globalColorGrading);
+    }
 
-        Spline x = new Spline(new AnimationCurve(), 2, false, new Vector2(0.2f, 0.2f) );
+    public void DesaturateEverything()
+    {
+        Spline initialCurve = _globalColorGrading.hueVsSatCurve;
+        initialCurve.curve.AddKey(0.5f, 0);
 
-        colorGrading.hueVsSatCurve.value = x;
+        _globalColorGrading.hueVsSatCurve.value = initialCurve;
+    }
+    
+    //TODO: Smooth transitions
+    public void SaturateProfile(int index) {
+        
+        ColorGrading addedColorGrading;
+        profiles[index].TryGetSettings(out addedColorGrading);
+        AnimationCurve addedCurve = addedColorGrading.hueVsSatCurve.value.curve;
+        
+        if (_colorVirginity)
+        {
+            _globalColorGrading.hueVsSatCurve.value = addedColorGrading.hueVsSatCurve.value;
+            _colorVirginity = false;
+        }
+        else
+        {
+            Spline globalInitialCurve = _globalColorGrading.hueVsSatCurve.value;
+            foreach (var key in addedCurve.keys)
+            {
+                globalInitialCurve.curve.AddKey(key);
+            }
+
+            _globalColorGrading.hueVsSatCurve.value = globalInitialCurve;
+        }
+        
     }
 
     private void Update() {
         // TODO: Delete
         if (Input.GetKeyDown("q")) {
-            DeSaturateGreen();
-        }
-
-        if (Input.GetKeyDown("w")) {
-
+            DesaturateEverything();
         }
     }
 }
